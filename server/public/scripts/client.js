a3f4142944d563b192ff748b2e08f6f5a92c057e
Josie Fredericksen <jfredericksen12@gmail.com>
2020-11-12 19:18:40 -0600

    Set up Get & Post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,11 +11,58 @@ function readyNow(){
 
 function getCalc(){
     console.log('clicked equal');
-    let number1 = $('#num1').val();
-    let number2 = $('#num1').val();
+    let value1 = $('#num1').val();
+    let value2 = $('#num2').val();
 
-    $.ajax({
+   $.ajax({
+        method: 'POST',
+        url: '/calc',
+        data: {
+           number1 : value1,
+           number2: value2
+        }
+    }).then(function(response) { //once you get a response run getCats
+        //The is run if you get a good response from the server
+        console.log('Added successfully');
+       //Get all the cats again, so we can see 
+        doMath();
+         //empty
+       $('#num1').val('');
+       $('#num2').val('');
         
-
+    }).catch(function(error){
+        //Catch runs if there is a bad response from the server
+        //console.log & alert the user
+        console.log('Error', error); //I see 
+        alert('Something bad happened. Try again later.'); //user sees
     })
+
+   function doMath(){
+        //Making a GET request to our server 
+        //This returns back a 'Promise' (object within JS)
+        $.ajax({
+           method: 'GET', 
+           url: '/calc' 
+        }).then( function(response) { //once you get a response THEN you'll run this function
+            console.log('Got response', response);
+            // renderCalculationHistory(response); COME BACK TO THIS
+        }).catch(function(error){
+            //console.log & alert the user
+            console.log('Error', error); //I see 
+            alert('Something bad happened. Try again later.'); //user sees
+        })
+    
+    }
 }
+
+// //start renderCalculationHistory
+// function renderCalculationHistory(calculatorArray){
+//     console.log('CalculatorArray is', calculatorArray;
+//     //empty (not important now, but will be later)
+//     $('#cat-list').empty();
+//     //put the cats into the ul
+//     for (let item of catArray){
+//         $('#cat-list').append(`<li>${item}</li>`);
+//     }//end for loop
+  
+// }//end renderCats
