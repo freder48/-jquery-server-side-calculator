dba7e7c8ff534e8794e71cb2eb9f908b4f6e692c
Josie Fredericksen <jfredericksen12@gmail.com>
2020-11-16 11:38:35 -0600

    Added Heroku Port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000; 
+const port = process.env.PORT || 5000;
 app.use( express.static('server/public') );
 app.use( bodyParser.urlencoded( {extended:true} ) );
 
